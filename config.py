fa0f4a0f1f3a7660be571ff7b7abd20125c3a41b
Dhruvv..? <154596608+dhruv4sure@users.noreply.github.com>
2023-12-22 16:43:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+{
+    "token": "MTE4NzAwMTkyODY5ODI5ODQ0OA.G5LBq7.wRWQ2SunkVSKBEdFp_k2Aw13Pzjr--m4eBtTGE",
+    "proxy": false,
+    "nuke": {
+        "channels_name": [
+            "Dhruvv x Team NUK3RSS",
+            "Server Beamedd",
+            "Team NUK3RSS",
+            "NUK3RSS Runs the Cord"
+        ],
+        "roles_name": [
+            "Server Beamedd",
+            "Team NUK3RSS",
+            "Dhruv x Team NUK3RSS"
+        ],
+        "messages_content": [
+            "@everyone @here **Dhruvv x Team NUK3RSS** https://discord.gg/nukerop",
+            "@everyone @here **Dhruvv x Team NUK3RSS** https://discord.gg/ntontop",
+            "@everyone @here **Dhruvv x Team NUK3RSS** https://discord.gg/ntontop"
+        ]
+    }
+}
